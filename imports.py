8d7481695489bdb5fbcb2a693aad10a46ad40ccc
Henrik Abrahamsson <he6285ab-s@student.lu.se>
2020-07-11 16:09:51 +0200

    prime factorization

--- imports.py
+++
@@ -1,9 +0,0 @@
-import math
-import cmath 
-import numpy
-
-
-def inv(x):
-	x = numpy.array(x)
-	y = numpy.linalg.inv(x)
-	print(y)
---
+++ pylab.py
@@ -0,0 +1,41 @@
+from math import *
+import cmath 
+import numpy
+
+# ---- Shortcut to inverse -----
+def inv(x):
+	x = numpy.array(x)
+	y = numpy.linalg.inv(x)
+	print(y)
+
+
+# ---- Find prime factors ------
+def factor(x):
+	number = x
+	primefactors = []
+	stop_index = 0
+
+	while stop_index != -1:
+		factor = _find_one_prime_factor(number)
+		if factor != -1:
+			primefactors.append(factor)
+			number //= factor # integer division due to python float conversion on division
+		else:
+			stop_index = factor
+
+	# If prime number is inserted, it will inevitably 
+	# be added to prime factor list exactly once.
+	# Remove it if it exists.
+	if x in primefactors:
+		primefactors.remove(x)
+
+	print("Prime factors:", primefactors)
+
+# helper method
+def _find_one_prime_factor(number):
+	for num in range (2, number+1):
+		for i in range(2, num+1):
+			if(number % i == 0):
+				return i
+
+	return -1
